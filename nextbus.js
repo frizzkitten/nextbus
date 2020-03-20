eeb613714bbf58446998c4cc00eb279917546ce3
Austin Meyer <frizzkitten@gmail.com>
2020-03-20 14:10:01 -0500

    prettier and gitignore

--- nextbus.js
+++ nextbus.js
@@ -2,72 +2,72 @@ const axios = require("axios");
 
 // get and then print the number of minutes until the next bus
 get_time_until_bus()
-  .then(minutes => {
-    console.log(`${minutes} Minutes`);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+    .then(minutes => {
+        console.log(`${minutes} Minutes`);
+    })
+    .catch(error => {
+        console.log(error);
+    });
 
 async function get_time_until_bus() {
-  return new Promise(async (resolve, reject) => {
-    // extract the info given by the user
-    try {
-      var [route, stop, direction] = get_user_input();
-    } catch (error) {
-      return reject(error);
-    }
-    console.log("route:", route);
+    return new Promise(async (resolve, reject) => {
+        // extract the info given by the user
+        try {
+            var [route, stop, direction] = get_user_input();
+        } catch (error) {
+            return reject(error);
+        }
+        console.log("route:", route);
 
-    // get the route id of the wanted route
-    // try {
-    //   const route_id = await get_route_id(route);
-    //   console.log("route_id: ", route_id);
-    // } catch (error) {
-    //   console.log("error: ", error);
-    //   return reject("Invalid route name.");
-    // }
+        // get the route id of the wanted route
+        // try {
+        //   const route_id = await get_route_id(route);
+        //   console.log("route_id: ", route_id);
+        // } catch (error) {
+        //   console.log("error: ", error);
+        //   return reject("Invalid route name.");
+        // }
 
-    // TODO: get the stop id (value)
+        // TODO: get the stop id (value)
 
-    // TODO: get the departures leaving from that stop
+        // TODO: get the departures leaving from that stop
 
-    // TODO: get the amount of time until the next departure
-  });
+        // TODO: get the amount of time until the next departure
+    });
 }
 
 // returns the first three user arguments
 function get_user_input() {
-  console.log("process.argv: ", process.argv);
-  // get the command line arguments
-  const command_line_args = process.argv.slice(2);
-  // ensure there are three of them - bus route, stop name, and direction
-  if (command_line_args.length < 3)
-    throw `Not enough arguments. Usage: node nextbus.js "BUS ROUTE" "BUS STOP NAME" "DIRECTION"`;
+    console.log("process.argv: ", process.argv);
+    // get the command line arguments
+    const command_line_args = process.argv.slice(2);
+    // ensure there are three of them - bus route, stop name, and direction
+    if (command_line_args.length < 3)
+        throw `Not enough arguments. Usage: node nextbus.js "BUS ROUTE" "BUS STOP NAME" "DIRECTION"`;
 
-  return command_line_args.slice(0, 3);
+    return command_line_args.slice(0, 3);
 }
 
 // TODO: takes in the name of a route and returns its id
 async function get_route_id(route_string) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      var routes = await axios.get(
-        "http://svc.metrotransit.org/NexTrip/Routes"
-      );
-    } catch (error) {
-      return reject(error);
-    }
+    return new Promise(async (resolve, reject) => {
+        try {
+            var routes = await axios.get(
+                "http://svc.metrotransit.org/NexTrip/Routes"
+            );
+        } catch (error) {
+            return reject(error);
+        }
 
-    console.log("routes: ", routes);
-    return resolve("jangus");
-  });
+        console.log("routes: ", routes);
+        return resolve("jangus");
+    });
 }
 
 // TODO: takes in a bus departure object and returns the amount of time
 // before that bus leaves
 function get_minutes_remaining() {
-  return 0;
+    return 0;
 }
 
 // axios
@@ -93,6 +93,6 @@ function get_minutes_remaining() {
 //   });
 
 module.exports = {
-  get_time_until_bus,
-  get_user_input
+    get_time_until_bus,
+    get_user_input
 };
--- test/nextbus.js
+++ test/nextbus.js
@@ -3,49 +3,49 @@ const { expect } = require("chai");
 const { get_time_until_bus, get_user_input } = require("../nextbus");
 
 const DEFAULT_ARGV = [
-  "/Users/username/.nvm/versions/node/v10.15.1/bin/node",
-  "/Users/username/target/nextbus.js"
+    "/Users/username/.nvm/versions/node/v10.15.1/bin/node",
+    "/Users/username/target/nextbus.js"
 ];
 
 describe("get_user_input", () => {
-  it("should throw an error if there are fewer than three args", () => {
-    const NOT_ENOUGH_ARGS_ERROR = `Not enough arguments. Usage: node nextbus.js "BUS ROUTE" "BUS STOP NAME" "DIRECTION"`;
-    const prev_process_argv = [...process.argv];
+    it("should throw an error if there are fewer than three args", () => {
+        const NOT_ENOUGH_ARGS_ERROR = `Not enough arguments. Usage: node nextbus.js "BUS ROUTE" "BUS STOP NAME" "DIRECTION"`;
+        const prev_process_argv = [...process.argv];
 
-    // 0 user args
-    process.argv = [...DEFAULT_ARGV];
-    expect(get_user_input).to.throw(NOT_ENOUGH_ARGS_ERROR);
+        // 0 user args
+        process.argv = [...DEFAULT_ARGV];
+        expect(get_user_input).to.throw(NOT_ENOUGH_ARGS_ERROR);
 
-    // 1 user arg
-    process.argv.push("route name");
-    expect(get_user_input).to.throw(NOT_ENOUGH_ARGS_ERROR);
+        // 1 user arg
+        process.argv.push("route name");
+        expect(get_user_input).to.throw(NOT_ENOUGH_ARGS_ERROR);
 
-    // 2 user args
-    process.argv.push("stop name");
-    expect(get_user_input).to.throw(NOT_ENOUGH_ARGS_ERROR);
+        // 2 user args
+        process.argv.push("stop name");
+        expect(get_user_input).to.throw(NOT_ENOUGH_ARGS_ERROR);
 
-    // cleanup
-    process.argv = prev_process_argv;
-  });
+        // cleanup
+        process.argv = prev_process_argv;
+    });
 
-  it("should return three user args if there are three or more args", () => {
-    const prev_process_argv = [...process.argv];
+    it("should return three user args if there are three or more args", () => {
+        const prev_process_argv = [...process.argv];
 
-    // 3 user args
-    process.argv = [...DEFAULT_ARGV, "route", "stop", "direction"];
-    expect(get_user_input()).to.include.ordered.members([
-      "route",
-      "stop",
-      "direction"
-    ]);
+        // 3 user args
+        process.argv = [...DEFAULT_ARGV, "route", "stop", "direction"];
+        expect(get_user_input()).to.include.ordered.members([
+            "route",
+            "stop",
+            "direction"
+        ]);
 
-    // 4 user args
-    process.argv.push("additional arg");
-    expect(get_user_input())
-      .to.include.ordered.members(["route", "stop", "direction"])
-      .and.to.have.lengthOf(3);
+        // 4 user args
+        process.argv.push("additional arg");
+        expect(get_user_input())
+            .to.include.ordered.members(["route", "stop", "direction"])
+            .and.to.have.lengthOf(3);
 
-    // cleanup
-    process.argv = prev_process_argv;
-  });
+        // cleanup
+        process.argv = prev_process_argv;
+    });
 });
