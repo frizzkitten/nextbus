8e0396b8f08e98ee1cafefa5cdf15aeb8b5c7221
Austin Meyer <frizzkitten@gmail.com>
2020-03-20 11:42:43 -0500

    get command line input

--- nextbus.js
+++ nextbus.js
@@ -1,10 +1,26 @@
 const axios = require("axios");
 
-axios
-  .get("https://svc.metrotransit.org/NexTrip/Providers")
-  .then(response => {
-    console.log("response.data: ", response.data);
-  })
-  .catch(error => {
-    console.log("error: ", error);
-  });
+get_time_until_bus();
+
+function get_time_until_bus() {
+  // get the command line arguments
+  const command_line_args = process.argv.slice(2);
+  // ensure there are three of them - bus route, stop name, and direction
+  if (command_line_args.length < 3)
+    return console.log(
+      `Not enough arguments. Usage: node nextbus.js "BUS ROUTE" "BUS STOP NAME" "DIRECTION"`
+    );
+  // extract the needed info
+  const [route, stop, direction] = command_line_args;
+
+  console.log("args:", command_line_args);
+}
+
+// axios
+//   .get("https://svc.metrotransit.org/NexTrip/Providers")
+//   .then(response => {
+//     console.log("response.data: ", response.data);
+//   })
+//   .catch(error => {
+//     console.log("error: ", error);
+//   });
