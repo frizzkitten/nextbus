388ef884d61a0fc7e6697ce34cc34cc221e884a3
Austin Meyer <frizzkitten@gmail.com>
2020-03-20 13:57:41 -0500

    skeleton code to get to the point where it returns a time correctly

--- nextbus.js
+++ nextbus.js
@@ -1,15 +1,29 @@
 const axios = require("axios");
 
-try {
-  get_time_until_bus();
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   get_time_until_bus();
+// } catch (error) {
+//   console.log(error);
+// }
 
-function get_time_until_bus() {
+async function get_time_until_bus() {
   // extract the needed info
   const [route, stop, direction] = get_user_input();
   console.log("route:", route);
+
+  // TODO: get the route id of the wanted route
+  try {
+    const route_id = get_route_id(route);
+  } catch (error) {
+    console.log("error: ", error);
+    throw "Invalid route name.";
+  }
+
+  // TODO: get the stop id (value)
+
+  // TODO: get the departures leaving from that stop
+
+  // TODO: get the amount of time until the next departure
 }
 
 // returns the first three user arguments
@@ -24,14 +38,38 @@ function get_user_input() {
   return command_line_args.slice(0, 3);
 }
 
-// axios
-//   .get("https://svc.metrotransit.org/NexTrip/Providers")
-//   .then(response => {
-//     console.log("response.data: ", response.data);
-//   })
-//   .catch(error => {
-//     console.log("error: ", error);
-//   });
+// TODO: takes in the name of a route and returns its id
+function get_route_id(route_string) {
+  return 0;
+}
+
+// TODO: takes in a bus departure object and returns the amount of time
+// before that bus leaves
+function get_minutes_remaining() {
+  return 0;
+}
+
+axios
+  .get("http://svc.metrotransit.org/NexTrip/903/4/CGTR")
+  .then(response => {
+    console.log("response.data: ", response.data);
+    const departures = response.data;
+    departures.forEach(d => {
+      // get the timestamp in milliseconds for when the bus leaves
+      const millis = parseInt(d.DepartureTime.substring(6, 19), 10);
+      // get the number of milliseconds until the bus leaves
+      const now = new Date();
+      const remaining_millis = millis - now.getTime();
+
+      // return the number of milliseconds left converted into minutes
+      console.log("minutes left: ", remaining_millis / 60000);
+
+      console.log("");
+    });
+  })
+  .catch(error => {
+    console.log("error: ", error);
+  });
 
 module.exports = {
   get_time_until_bus,
